0eba6a80bfd0775a9a762f6bb4f04f0b77fd692c
Lyramor <mmarsa2435@gmail.com>
2025-02-16 18:31:58 +0700

    Menghapus Debuging

--- inc/koneksi.php
+++ inc/koneksi.php
@@ -9,27 +9,4 @@
   if (!file_exists('upload/profile')) {
     mkdir('upload/profile', 0777, true);
   }
-
-  // Debug session
-var_dump($_SESSION);
-
-// Debug parameter
-var_dump($_GET);
-
-// Debug artikel_id
-if (isset($_GET['p'])) {
-    $artikel_id = $_GET['p'];
-    echo "ID Artikel: " . $artikel_id;
-    
-    // Debug query
-    $query = "SELECT * FROM artikel WHERE id = '$artikel_id'";
-    $result = mysqli_query($conn, $query);
-    
-    if (!$result) {
-        echo "Error query: " . mysqli_error($conn);
-    } else {
-        $artikel = mysqli_fetch_assoc($result);
-        var_dump($artikel);
-    }
-}
 ?>
